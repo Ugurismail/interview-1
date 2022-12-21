625ac35c070e81c93016c11eec3430310e2007ce
Ugurismail <uguraygun001@hotmail.com>
2022-12-21 17:43:24 +0300

    final

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+//just to add little bir of js
+const items = [...document.querySelectorAll(".slide")];
+
+let options = {
+	margin: "-10%",
+	threshold: 0.0,
+};
+
+let observer = new IntersectionObserver(showItem, options);
+
+function showItem(entries) {
+	entries.forEach((el) => {
+		if (el.isIntersecting) {
+			console.log(el);
+			el.target.classList.remove("slide");
+			el.target.classList.add("active");
+		}
+	});
+}
+
+items.forEach((item) => {
+	observer.observe(item);
+});
